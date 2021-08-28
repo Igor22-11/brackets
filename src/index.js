8fb92e547d43e012ab05ee0fe5b32b97cb36d99b
Igor Bespalov <78505241+Igor22-11@users.noreply.github.com>
2021-08-28 20:20:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    const newBrackets = bracketsConfig.map((num)=> num.join(''));
+
+    function replaceBrackets(string) {
+        // const lengthBracketsConfig=newBrackets.length
+        for (let i = 0; i < newBrackets.length; i++) {
+            if (string.includes(newBrackets[i])) {
+                string = string.replace(newBrackets[i], '');
+                return replaceBrackets(string);
+            } else if (string.length === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return replaceBrackets(str);
 }
